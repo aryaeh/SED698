6f0f2c298caf26f8eb7151cf9de2ea1725696276
aryaeh <38204369+aryaeh@users.noreply.github.com>
2023-04-09 14:50:48 +0700

    Add files via upload

--- app_predict_penguin.py
+++ app_predict_penguin.py
@@ -1,4 +1,4 @@
-
+import plotly.graph_objects as px
 import numpy as np
 import pandas as pd
 import pickle
@@ -11,23 +11,53 @@ species_encoder = pickle.load(open('encoder.species.sav','rb'))
 island_encoder = pickle.load(open('encoder.island.sav','rb'))
 sex_encoder = pickle.load(open('encoder.sex.sav','rb'))
 evaluations = pickle.load(open('evals.all.sav','rb'))
+st.set_page_config(
+    page_title="Penguin",
+    layout="wide"
+)
 
-st.title('Penguin Species Prection :) ')
+st.title("Penguin Species Predition")
 
-x1 = st.radio('Select island', island_encoder.classes_)
-x1 = island_encoder.transform([x1])[0]
-# x1 
-x2 = st.slider('Select culmen length (mm)', 25, 70, 40)
-x3 = st.slider("เลือก culmen depth (mm)", 10,30,15 )
-x4 = st.slider("เลือก flipper length (mm)", 150,250,200)
-x5 = st.slider("เลือก body mass (g)", 2500,6500,3000)
-x6 = st.radio("เลือก sex ",sex_encoder.classes_)
-x6 = sex_encoder.transform([x6])[0]
+tab1,tab2 = st.tabs(["Prediction","Evaluation"])
+with tab1:
+    st.header('Prediction')
 
-x_new = pd.DataFrame(data=np.array([x1, x2, x3, x4, x5, x6]).reshape(1,-1), 
-             columns=['island', 'culmen_length_mm', 'culmen_depth_mm','flipper_length_mm', 'body_mass_g', 'sex'])
+    x1 = st.radio('Select island', island_encoder.classes_)
+    x1 = island_encoder.transform([x1])[0]
+    # x1 
+    x2 = st.slider('Select culmen length (mm)', 25, 70, 40)
+    x3 = st.slider("เลือก culmen depth (mm)", 10,30,15 )
+    x4 = st.slider("เลือก flipper length (mm)", 150,250,200)
+    x5 = st.slider("เลือก body mass (g)", 2500,6500,3000)
+    x6 = st.radio("เลือก sex ",sex_encoder.classes_)
+    x6 = sex_encoder.transform([x6])[0]
 
-pred = model.predict(x_new)
+    x_new = pd.DataFrame(data=np.array([x1, x2, x3, x4, x5, x6]).reshape(1,-1), 
+                 columns=['island', 'culmen_length_mm', 'culmen_depth_mm','flipper_length_mm', 'body_mass_g', 'sex'])
 
-st.write('Predicted Species: ' , species_encoder.inverse_transform(pred)[0])
+    pred = model.predict(x_new)
 
+    st.write('Predicted Species: ' , species_encoder.inverse_transform(pred)[0])
+with tab2:
+    st.header('Evaluation on Five Technique')
+    x = evaluations.columns
+    fig = px.Figure(data=[
+        px.Bar(name = 'Decision Tree',
+               x = x,
+               y = evaluations.loc['Decision Tress']),
+        px.Bar(name = 'Random Forest',
+               x = x,
+               y =  evaluations.loc['Random Forest']),
+        px.Bar(name = 'KNN',
+               x = x,
+               y =  evaluations.loc['KNN']),
+        px.Bar(name = 'AdaBoost',
+               x = x,
+               y =  evaluations.loc['AdaBoost']),
+        px.Bar(name = 'XGBoost',
+               x = x,
+               y =  evaluations.loc['XGBoost'])
+    ])
+    st.plotly_chart(fig, use_container_width=True)
+
+    st.dataframe(evaluations)    
